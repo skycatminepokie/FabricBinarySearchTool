ff9422f9f3f40b9bdb7f725c0566003fbd5b888c
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-11-22 22:11:46 -0800

    Bisect fleshed out
    Doesn't have choice algorithm implemented.

--- src/main/java/com/skycatdev/binarysearchtool/Mod.java
+++ src/main/java/com/skycatdev/binarysearchtool/Mod.java
@@ -1,10 +1,20 @@
 package com.skycatdev.binarysearchtool;
 
+import java.nio.file.Path;
+import java.util.Set;
+
 /**
  *
  * @param id The modid of the mod.
  * @param dependencies The dependencies listed by the mod.
  * @param filename The name of the mod file, not including extensions.
  */
-public record Mod(String id, String[] dependencies, String filename) {
+public record Mod(String id, Set<String> dependencies, String filename) {
+    public boolean tryDisable(Path modFolder) {
+        return modFolder.resolve(filename + ".jar").toFile().renameTo(modFolder.resolve(filename + ".jar.DISABLED").toFile());
+    }
+
+    public boolean tryEnable(Path modFolder) {
+        return modFolder.resolve(filename + ".jar.DISABLED").toFile().renameTo(modFolder.resolve(filename + ".jar").toFile());
+    }
 }
--- src/main/java/com/skycatdev/binarysearchtool/SearchGui.java
+++ src/main/java/com/skycatdev/binarysearchtool/SearchGui.java
@@ -1,5 +1,9 @@
 package com.skycatdev.binarysearchtool;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.JFrame;
@@ -9,19 +13,24 @@ import java.awt.BorderLayout;
 import javax.swing.JButton;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
-import java.io.File;
+import java.io.*;
 import java.nio.file.FileSystems;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 import javax.swing.JSplitPane;
 import javax.swing.JProgressBar;
 import javax.swing.JTextPane;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class SearchGui extends JFrame {
+    public static final Gson GSON = new Gson();
     private final JTextPane instructionsPane;
     private final JPanel bottomPanel;
     private final JButton undoButton;
@@ -39,6 +48,9 @@ public class SearchGui extends JFrame {
     private JTextField pathField;
     private @Nullable Path modsPath;
     private final ArrayList<Mod> mods = new ArrayList<>();
+    private final ArrayList<Mod> workingMods = new ArrayList<>();
+    private final ArrayList<Mod> candidateMods = new ArrayList<>();
+    private final ArrayList<Mod> testingMods = new ArrayList<>();
     private boolean searching = false;
 
     /**
@@ -128,7 +140,102 @@ public class SearchGui extends JFrame {
         // modsPath is initialized
         // populate mods
         File[] possibleModFiles;
+        try {
+            possibleModFiles = modsPath.toFile().listFiles(file -> file.getPath().endsWith(".jar"));
+        } catch (SecurityException e) {
+            // TODO: Could not access
+            return;
+        }
+        if (possibleModFiles == null) {
+            // TODO: I/O error
+            return;
+        }
+        for (File possibleModFile : possibleModFiles) {
+            try (JarFile jarFile = new JarFile(possibleModFile)) {
+                JarEntry fmj = jarFile.getJarEntry("fabric.mod.json");
+                if (fmj == null) {
+                    // TODO: Maybe warn that it's not a mod
+                    continue;
+                }
+                try (InputStream inputStream = jarFile.getInputStream(fmj)) {
+                    JsonObject fmjJson = JsonParser.parseReader(new InputStreamReader(inputStream)).getAsJsonObject();
+                    JsonElement dependsElement = fmjJson.get("depends");
+                    Set<String> dependencies;
+                    if (dependsElement != null) {
+                        dependencies = dependsElement.getAsJsonArray()
+                                .asList()
+                                .stream()
+                                .map((JsonElement::getAsString))
+                                .collect(Collectors.toSet());
+                    } else {
+                        dependencies = Collections.emptySet();
+                    }
+                    String id = fmjJson.get("id").getAsString();
+                    String fileName = possibleModFile.getName();
+                    int extensionIndex = fileName.lastIndexOf(".jar") - 1;
+                    if (extensionIndex == -1) {
+                        extensionIndex = fileName.length();
+                    }
+                    mods.add(new Mod(id, dependencies, fileName.substring(0, extensionIndex)));
+                }
+            } catch (IOException e) {
+                // TODO: JarFile error
+            }
+        }
+        candidateMods.addAll(mods);
+        for (Mod testingMod : testingMods) {
+            if (!testingMod.tryDisable(modsPath)) {
+                // TODO: Warning with instructions to fix
+            }
+        }
+        searching = true;
+        bisect(true, 1);
+    }
+
+    /**
+     *
+     * @param lastSuccessful If the last set was successful (error is gone)
+     * @param iteration The iteration of this step
+     */
+    private void bisect(boolean lastSuccessful, int iteration) {
+        assert modsPath != null;
+        if (lastSuccessful) {
+            workingMods.addAll(testingMods);
+        } else {
+            workingMods.addAll(candidateMods);
+            workingMods.removeAll(testingMods);
+            candidateMods.clear();
+            candidateMods.addAll(testingMods);
+        }
+        for (Mod testingMod : testingMods) {
+            if (!testingMod.tryDisable(modsPath)) {
+                // TODO: Warning with instructions to fix
+            }
+        }
+        testingMods.clear();
+        // Ready for next step
+
+        // TODO: Choose mods to use
+
+        for (Mod testingMod : testingMods) {
+            if (!testingMod.tryEnable(modsPath)) {
+                // TODO: Warning with instructions to fix
+            }
+        }
+
+        updateLists();
+        updateProgress();
+        instructionsPane.setText("Next step is ready! Launch Minecraft, test (or crash), then close it (or crash). If the error is gone, press Success. If it's still there, press Failure.");
+    }
+
+    private void updateProgress() {
         // TODO
     }
+
+    private void updateLists() {
+        // TODO
+    }
+
+    // TODO: When finished, enable all mods
 
 }
